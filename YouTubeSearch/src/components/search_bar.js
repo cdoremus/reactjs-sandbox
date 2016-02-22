c8da4ebd648d14de239e7142b382904fab914378
Craig Doremus <Craig.Doremus@altarum.org>
2016-02-22 17:35:25 -0500

    Started with YouTube search API. Getting 400 error.

--- YouTubeSearch/src/components/search_bar.js
+++ YouTubeSearch/src/components/search_bar.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Component } from 'react';
 
+
 export default class SearchBar extends Component {
     
     constructor(props) {
         super(props);
-        
+        this.state = {searchInput:''}
     }
     
     render() {
         return (
-        <input onChange={this.onInputChange}/>
+            <div>
+                <input 
+                    value={this.state.searchInput}
+                    onChange={event => this.setState({searchInput: event.target.value})}/>
+                Searching for: {this.state.searchInput}
+            </div>
         );
     }
     
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
 }
 
--- YouTubeSearch/src/index.js
+++ YouTubeSearch/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDAHh4H48TiIHe_lN1cgQUhab5uaZA';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
 
 const App = () => {
     return <div><SearchBar/></div>    
